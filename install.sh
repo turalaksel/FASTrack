4b1948fe6398475c2e4d6cfc1459b8805231c2d4
turalaksel <turalaksel@gmail.com>
2016-08-10 08:34:54 -0700

    Write environment variables into bashrc

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #If bash profile file doesn't exist create one
-if ! [ -f ~/.profile ]
+if ! [ -f ~/.bashrc ]
 then
-	touch ~/.profile
+	touch ~/.bashrc
 fi
 
 #Delete any FAST installation lines
-sed -i -e '/^#Adding FAST_v/{N;d;}' ~/.profile
+sed -i -e '/^#Adding FAST_v/{N;d;}' ~/.bashrc
 
 #Write FAST installation lines
-echo -e "\n#Adding FAST_v1.0/bin directory to PATH\nexport PATH=\"${PWD}/bin:\$PATH\"\n" >> ~/.profile
-echo -e "\n#Adding FAST_v1.0 to PYTHONPATH\nexport PYTHONPATH=\"${PWD}:\$PYTHONPATH\"\n" >> ~/.profile
+echo -e "\n#Adding FAST_v1.0/bin directory to PATH\nexport PATH=\"${PWD}/bin:\$PATH\"\n" >> ~/.bashrc
+echo -e "\n#Adding FAST_v1.0 to PYTHONPATH\nexport PYTHONPATH=\"${PWD}:\$PYTHONPATH\"\n" >> ~/.bashrc
 
 chmod +x bin/*
